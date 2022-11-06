bef255ab33a18b1c096a9b5d582a1ffdfecfe09d
Marvin Velasco <marvinjvelasco@gmail.com>
2022-11-06 03:34:44 -0800

    added comments to javascrip

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,26 +1,30 @@
-// Shows current date
+// todayDate uses moment, a function that shows the current date and gives it a format
 var todayDate = moment().format('dddd, MMM Do YYYY');
+// this is supposed to display the todayDate var on the page
 $("#currentDay").html(todayDate);
 
+// If using jQuery, they must have a doc ready event to stop any code from running before it is finished loading
 $(document).ready(function () {
-    // Click Event Listener
+    // This is an event listener that is to save the note when clicking the save button
     $(".saveBtn").on("click", function () {
-        // Use nearby values for the description in Jquery
+        // Use nearby values for the description in Jquery, calling the description and id attributes
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Saves notes in local storage
+        // Saves notes in local storage, using the above vars as the values
         localStorage.setItem(time, text);
     })
-
+    // a funtion to loop through the time blocks to change to css according the to the current time
     function timeTracker() {
         // current number of hours
         var timeNow = moment().hour();
 
-        // loop for time blocks
+        // loop for time blocks, loops through each time block or hour
         $(".time-block").each(function () {
+            // parseInt turns a string into an integer for the below if else statements
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+            //  these are the loops for which of the css rules are to be applied
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
